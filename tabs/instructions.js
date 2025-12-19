e4bc129c55d42a71c737a5436fb1510cbafd6af7
Kobis09 <mujazemib@gmail.com>
2025-12-20 00:49:10 +0100

    make token contract copyable

--- tabs/instructions.js
+++ tabs/instructions.js
@@ -25,25 +25,25 @@ export default function Instructions() {
 
   const steps = [
     {
-      number: "01",
-      title: "Import the Token on MetaMask",
-      shortTitle: "Import Token",
-      description: "Add ILLYRIAN token to MetaMask to receive all payments and track your investments securely.",
-      details: [
-  "Open MetaMask and tap the '+' icon",
-  "Select the BNB Chain network and paste the Token contract address",
-  {
-    type: "copy",
-    label: "Token Contract",
-    value: "0xC9Aa04758559DAcf7C5D9e41ed28E3595cC8ED58",
-  },
-  "Click Import",
-  "Your ILLYRIAN balance will now be visible",
-],
-
-      icon: "📱",
-      color: "#8b5cf6",
+  number: "01",
+  title: "Import the Token on MetaMask",
+  shortTitle: "Import Token",
+  description: "Add ILLYRIAN token to MetaMask to receive all payments and track your investments securely.",
+  details: [
+    "Open MetaMask and tap the '+' icon",
+    "Select the BNB Chain network and paste the Token contract address",
+    {
+      type: "copy",
+      label: "Token Contract",
+      value: "0xC9Aa04758559DAcf7C5D9e41ed28E3595cC8ED58",
     },
+    "Click Import",
+    "Your ILLYRIAN balance will now be visible",
+  ],
+  icon: "📱",
+  color: "#8b5cf6",
+},
+
     {
       number: "02",
       title: "Provide Your Wallet Addresses",
